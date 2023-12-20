f012dfbc22b0336e4f52a7810ca6dc184c74c39b
Hector Fuentes <hgfuentes@falabella.cl>
2023-12-20 01:30:48 -0300

    chore: fix folder structure, include aws configuration files, fix default server port, add readme file

--- server/server.js
+++ server/server.js
@@ -11,5 +11,5 @@ app.get('/api/data', (req, res) => {
     res.json({ mensaje: 'Datos de ejemplo' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
